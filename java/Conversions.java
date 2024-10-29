34f63d87ee80d83127cd9abd1ecdcb57b42e3961
Admin <chellurivenkataajay@gmail.com>
2024-10-29 15:11:14 +0530

    chore:conversions class

---
+++ java/Conversions.java
@@ -0,0 +1,20 @@
+package com.conversions.java;
+
+public class Conversions {
+    public static void main(String[] args) {
+        //conversion between one data type to another data type
+        //automatic conversion or implicit conversion
+        int a=432;
+        double b;
+        b=a;
+        System.out.println(b);
+
+        //Explicit conversion or manual conversion
+
+        double c;
+        c=47.7654;
+        short d;
+        d= (short) c;
+        System.out.println(d);
+    }
+}
